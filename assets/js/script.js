4ab60b958ff3a6031244211dfa4d1794375da4d7
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-01-09 23:27:14 +0000

    Add footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 /* this script was created using the following tutorial:
-https://www.youtube.com/watch?v=DyqMglmrido and https://javascript.plainenglish.io/building-a-rock-paper-scissors-game-with-javascript-bce23d39509d. I have borrowed code from this tutorial to assist with creating the spock game and i also personalised some of the code for my own styling and created... */
+https://www.youtube.com/watch?v=lV2BMXdsDmc&t=1747s. I have borrowed code from this tutorial to assist with creating the spock game and i also personalised some of the code for my own styling and created... */
 
-// start the game 
-startGame();
-  
-function startGame() 
+
+
