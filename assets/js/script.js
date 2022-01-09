361128c50ba96070a38c1ff13ef2bd482104782c
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-01-09 08:00:01 +0000

    Add taken timer off

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,9 @@
 /* this script was created using the following tutorial:
 https://www.youtube.com/watch?v=DyqMglmrido and https://javascript.plainenglish.io/building-a-rock-paper-scissors-game-with-javascript-bce23d39509d. I have borrowed code from this tutorial to assist with creating the spock game and i also personalised some of the code for my own styling and created... */
 
-var timer; 
-var timeLeft = 5; // seconds
-
-// What to do when the timer runs out
-function gameOver() {
-  // This cancels the setInterval, so the updateTimer stops getting called
-  cancelInterval(timer);
+// start the game 
+startGame();
   
-  // re-show the button, so they can start it again
-  $('#playAgainButton').show();
-}
-
-function updateTimer() {
-  timeLeft = timeLeft - 1;
-  if(timeLeft >= 0)
-    $('#timer').html(timeLeft);
-  else {
-    gameOver();
-  }
-}
-
-      
-     
+function startGame() {
+    
+   
