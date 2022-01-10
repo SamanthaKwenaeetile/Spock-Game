29db916258e7a9cf55cd1985fb190d06f7919a35
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-01-10 01:42:19 +0000

    Add game score result

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,31 @@ let userplayer = 1;
 const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection button');
-const showIcon = document.querySelector('.show i');
-const computerShowIcon = document.querySelector('.computer i');
-// The randomClass array below this contains the rock,paper, and scissor Icon from font-awesome.
+const displayIcon = document.querySelector('.display i');
+const computerdisplayIcon = document.querySelector('.display i');
+// The randomClass array below this contains Icons from font-awesome.
 const randomClasses = ["far fa-hand-rock","far fa-hand-paper","far fa-hand-scissors","far fa-hand-lizard" ,"far fa-hand-spock" ];
 const text = document.getElementById('display');
 const text2 = document.getElementById('display2');
 
+
+// Game Functionality: Setting forEach function for the buttons.
+const game = () =>{
+    buttons.forEach(btn =>{
+        btn.addEventListener('click',(e)=>{
+        // Random rock paper scissor for the computer and the player
+           let clickedBtn = e.target.className;
+           displayIcon.className = clickedBtn;
+           let randomNum = Math.floor(Math.random() * randomClasses.length);
+           computerShowIcon.className = randomClasses[randomNum];
+           // Game Score.
+           // If it's a Tie .
+           if(showIcon.className === computerShowIcon.className){
+               pScore.innerHTML = pScore.innerHTML;
+               cScore.innerHTML = cScore.innerHTML;
+               text.innerHTML = "OH NO!! It's a Tie ! ";
+               text.style.color = 'black';
+               text2.innerHTML = text.innerHTML;
+               text2.style.color = 'black';
+           }
+        
