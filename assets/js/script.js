49bbfa0f0345d3fdda4ff462c1c03adfc29bf505
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-01-10 01:30:11 +0000

    Add randomclass array -contains icons from  font-awesome

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 /* this script was created using the following tutorial:
 https://www.youtube.com/watch?v=lV2BMXdsDmc&t=1747s. I have borrowed code from this tutorial to assist with creating the spock game and i also personalised some of the code for my own styling and created... */
 
+const selectionButtons = document.querySelectorAll('[data-selection]')
+
 
 
